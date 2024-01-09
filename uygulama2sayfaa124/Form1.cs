7bc5a55ff9ca9a25a6d8f18b0bc6bb71edec73d5
yeliz601 <yeliz601@BPC11>
2024-01-09 09:57:22 +0300

    Proje dosyası ekle.

---
+++ uygulama2sayfaa124/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama2sayfaa124
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] isimler = new string[5];
+        int index = 0;
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            isimler[index] = txtAdSoyad.Text;
+            index++;
+           
+        }
+
+        private void btnListe_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < isimler.Length; i++)
+            {
+                lbListe.Items.Add(isimler[i]);
+            }
+        }
+    }
+}
---
+++ uygulama2sayfaa124/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("uygulama2sayfaa124")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("uygulama2sayfaa124")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("20464018-7db0-4697-bf57-b50b0420f898")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
